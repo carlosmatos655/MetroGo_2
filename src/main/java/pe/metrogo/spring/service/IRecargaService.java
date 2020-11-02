f5bc5eed0ba465d8713ebc78f88ed57b6a1333d0
carlosmatos655 <40475823+carlosmatos655@users.noreply.github.com>
2020-11-01 22:16:38 -0500

    RecargaService

---
+++ src/main/java/pe/metrogo/spring/service/IRecargaService.java
@@ -0,0 +1,16 @@
+package pe.metrogo.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.metrogo.spring.entity.Recarga;
+
+public interface IRecargaService {
+	
+	public boolean insertar(Recarga recarga);
+	public boolean modificar(Recarga recarga);
+	public void eliminar(int CRecarga);
+	public Optional<Recarga> listarId(int CRecarga);
+	List<Recarga> listar();
+	List<Recarga> buscarPromocion(String NPromocion);
+}

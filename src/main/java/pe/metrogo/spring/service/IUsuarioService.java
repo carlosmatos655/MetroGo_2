0e1546734a201635086211b327b9393997706cb4
carlosmatos655 <40475823+carlosmatos655@users.noreply.github.com>
2020-11-23 22:24:43 -0500

    Recarga GetTime Correcto

--- src/main/java/pe/metrogo/spring/service/IUsuarioService.java
+++ src/main/java/pe/metrogo/spring/service/IUsuarioService.java
@@ -1,8 +1,15 @@
 package pe.metrogo.spring.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import pe.metrogo.spring.entity.Usuario;
 
 public interface IUsuarioService {
 	
 	public boolean insertar(Usuario usuario);
+	public boolean modificar(Usuario usuario);
+	public void eliminar(int id);
+	public Optional<Usuario> listarId(int id);
+	List<Usuario> listar();
 }
--- src/main/java/pe/metrogo/spring/serviceimpl/UsuarioServiceImpl.java
+++ src/main/java/pe/metrogo/spring/serviceimpl/UsuarioServiceImpl.java
@@ -1,5 +1,8 @@
 package pe.metrogo.spring.serviceimpl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -29,5 +32,35 @@ public class UsuarioServiceImpl implements IUsuarioService{
 			return false;
 		else
 			return true;
+	}
+
+	@Override
+	public boolean modificar(Usuario usuario) {
+		boolean flag= false;
+		try {
+			dUsuario.save(usuario);
+			flag = true;
+		} catch(Exception ex){
+			System.out.println("Sucedio un error");
+		}
+		return flag;
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+		dUsuario.deleteById(id);	
+	}
+
+	@Override
+	public Optional<Usuario> listarId(int id) {
+		// TODO Auto-generated method stub
+		return dUsuario.findById(id);
+	}
+
+	@Override
+	public List<Usuario> listar() {
+		// TODO Auto-generated method stub
+		return dUsuario.findAll();
 	}
 }
